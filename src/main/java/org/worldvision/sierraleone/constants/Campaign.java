91fd45c4ff85fa43f638250165dae74a46d81f1f
Rob LaRubbio <rob@larubbio.org>
2013-02-22 10:28:39 -0800

    Move constants to their own package

---
+++ src/main/java/org/worldvision/sierraleone/constants/Campaign.java
@@ -0,0 +1,14 @@
+package org.worldvision.sierraleone.constants;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class Campaign {
+    private Campaign() { }
+
+    private static final Logger logger = LoggerFactory.getLogger("org.worldvision.sierraleone.constants.Campaign");
+
+    public static final String POSTNATAL_CONSULTATION_REMINDER_CAMPAIGN = "Health Center Postnatal Consultation Reminder";
+    public static final String MOTHER_REFERRAL_REMINDER_CAMPAIGN = "Mother Referral Reminder";
+    public static final String CHILD_VITAMIN_A_REMINDER_CAMPAIGN = "Child Vitamin A Reminder";
+}
--- src/main/java/org/worldvision/sierraleone/constants/EventKeys.java
+++ src/main/java/org/worldvision/sierraleone/constants/EventKeys.java
@@ -1,4 +1,4 @@
-package org.worldvision.sierraleone;
+package org.worldvision.sierraleone.constants;
 
 import org.motechproject.event.MotechEvent;
 import org.slf4j.Logger;
@@ -10,7 +10,7 @@ import org.slf4j.LoggerFactory;
 public final class EventKeys {
     private EventKeys() { }
 
-    private static final Logger logger = LoggerFactory.getLogger("org.worldvision.sierraleone.EventKeys");
+    private static final Logger logger = LoggerFactory.getLogger("org.worldvision.sierraleone.constants.EventKeys");
 
     public static final String DATE_OF_BIRTH = "date_of_birth";
     public static final String DATE_OF_VISIT = "date_of_visit";
@@ -21,10 +21,13 @@ public final class EventKeys {
     public static final String ATTENDED_POSTNATAL = "attended_postnatal";
     public static final String PLACE_OF_BIRTH = "place_of_birth";
     public static final String MOTHER_CASE_ID = "mother_case_id";
+    public static final String CHILD_CASE_ID = "child_case_id";
+    public static final String VITAMIN_A = "vitamin_a";
 
     public static final String BASE_SUBJECT = "org.worldvision.sierraleone.";
     public static final String FORM_BASE_SUBJECT = BASE_SUBJECT + "form.";
     public static final String REGISTER_CHILD_FORM_SUBJECT = FORM_BASE_SUBJECT + "register-child";
+    public static final String CHILD_VISIT_FORM_SUBJECT = FORM_BASE_SUBJECT + "child-visit";
     public static final String POST_PARTUM_FORM_SUBJECT = FORM_BASE_SUBJECT + "post-partum";
     public static final String MOTHER_REFERRAL_SUBJECT = BASE_SUBJECT + "mother-referral";
 
